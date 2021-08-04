dfda19ba8b258fd852aa36b5ff323f677e1b6abe
Yu Ze <pseudoc@163.com>
2021-08-04 21:05:10 +0800

    format UT log

--- test/p_test.c
+++ test/p_test.c
@@ -12,7 +12,7 @@ int main() {
   
   for (int i = 0; i < n_cases; i++) {
     int exit_code = (uts[i])();
-    printf("UT %d: exit with %d.", i, exit_code);
+    printf("UT %d: exit with %d.\n", i, exit_code);
     if (exit_code)
       return exit_code;
   }
