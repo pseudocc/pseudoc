abfac8f440c852c2a7ba5581722e1f25bb350eea
pseudocc <pseudoc@163.com>
2021-09-07 22:26:41 +0800

    add descriptions

--- include/pseudoc/mm.h
+++ include/pseudoc/mm.h
@@ -5,15 +5,40 @@
 
 typedef struct memory_s memory_t;
 
+/**
+ * garbage collect
+ * 
+ * Try to merge the adjacent free pptr_t,
+ * and update the free memory pool.
+ **/
 static void pcollect(memory_t* mp);
+
 static void pmemcpy(pptr_t* dst, pptr_t* src);
 static inline void try_cc(memory_t* mp);
 
+/**
+ * memory_t constructor
+ **/
 memory_t* pminit();
+
+/**
+ * memory allocate
+ **/
 pptr_t* pmalloc(memory_t* mp, size_t n_bytes);
+
+/**
+ * memory re-allocate
+ **/
 pptr_t* prealloc(memory_t* mp, pptr_t* p, size_t n_bytes);
 
+/**
+ * mark certain pptr_t as free
+ **/
 void pfree(memory_t* mp, pptr_t* p);
+
+/**
+ * memory_t deconstructor
+ **/
 void pmclean(memory_t* mp);
 
 #endif
